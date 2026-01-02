c2759ebd0afd11c960332b6c6914d743e9df4307
Raisul516 <raisul.bracu@g.gmail.com>
2026-01-02 16:31:19 +0600

    changed api.js REACT_APP_API_URL to VITE_API_URL

--- finalone/worker-calling-frontend/src/services/api.js
+++ finalone/worker-calling-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5050';
+const API_URL = import.meta.VITE_API_URL || 'http://localhost:5050';
 
 // Create axios instance
 const api = axios.create({
